12a1bfd2972829dd55a7c06082e0b856b4435af0
Junyong Jung <thejunyongjung@gmail.com>
2026-04-27 14:35:12 -0700

    Build Home Screen Menu. Main.java can run the app.

--- src/main/java/com/pluralsight/LedgerApp.java
+++ src/main/java/com/pluralsight/LedgerApp.java
@@ -6,19 +6,23 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LedgerApp {
     // Field
     private ArrayList<Transaction> transactions;
+    private Scanner scanner;
 
     // Constructor
     public LedgerApp() {
         this.transactions = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
     }
 
-    // Method: Running the Applicaion
+    // Method: Running the LedgerApp
     public void run() {
         loadTransactions();
+        homeScreen();
     }
 
     // Method: Reading CSV file
@@ -42,5 +46,54 @@ public class LedgerApp {
         } catch (IOException e) {
             System.out.println("Error loading transactions! " + e.getMessage());
         }
+    }
+
+    // Method: Homescreen
+    private void homeScreen() {
+        boolean running = true;
+        while (running) {
+            System.out.println();
+            System.out.println("==============================");
+            System.out.println("Home Screen");
+            System.out.println("==============================");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            System.out.println("------------------------------");
+            System.out.print("Enter your choice: ");
+            String choice = scanner.nextLine().trim().toUpperCase();
+
+            switch (choice) {
+                case "D":
+                    addDeposit();
+                    break;
+                case "P":
+                    makePayment();
+                    break;
+                case "L":
+                    ledgerScreen();
+                    break;
+                case "X":
+                    System.out.println();
+                    System.out.println("Exiting the Application...");
+                    running = false;
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Invalid choice. Try again!");
+                    break;
+            }
+        }
+        System.out.println("Thank you");
+    }
+
+    private void addDeposit() {
+    }
+
+    private void makePayment() {
+    }
+
+    private void ledgerScreen() {
     }
 }
